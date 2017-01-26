2ed282c2c60ce181b76ff13f7dcdeeb4a470a2ba
radislavsplichal <radislavsplichal@MacBook-Pro-3.local>
2017-01-26 11:39:45 +0100

    Redefined forntend
    Changed form. Database model changed.
    
    Known Issue. Backend Doesn’t work.

--- index.php
+++ index.php
@@ -9,14 +9,50 @@ include 'databaseConnection.php';
     </head>
     <div class="row col-sm-3 col-xs-0"></div>
     <div class=" col-sm-6 ">
-        <form class="form-horizontal" action="saveItem.php" method="post">
-            
+        
+        <div class="page-header">
             <h1>Nákupní Seznam</h1> 
             <p>Do formuláře zapiš věci, které se mají nakoupit. Nezapomeň to uložit.</p>
+        </div>
+        
+        <form class="form-horizontal" action="saveItem.php" method="post">
             
-            <textarea name="text" class="form-control" rows="3" required ></textarea>
+            <div class="form-group">
+                <label for="nSeznamu">Název seznamu: </label>
+                <input class="form-control" id="nSeznamu" name="listName" required>
+            </div>
+            <div class="row">
+                 
+                <div class="form-group col-sm-6">
+                    <label for="txtPolozky">Položky: </label>
+                    <input class="form-control" id="txtPolozky" type="text" name="itemName">
+                </div>
+
+                <div class="form-group col-sm-3">
+                    <label for="pocetKus">Počet kusů:</label>
+                    <input class="form-control" id="pocetKus" type="number" name="quantity">
+                </div>
+
+                <div class="form-group col-sm-3">
+                    <label for="cenaKus">Cena za kus:</label>
+                    <input class="form-control" id="cenaKus" type="number" name="unitPrice">
+                </div>  
+                
+                <div class="glyphicon glyphicon-plus col-sm-1"></div>
+
+            </div>
+             
+            
+            <div class="form-group">
+                <label for="note">Poznámky: </label>
+                <textarea class="form-control" id="note" name="text" class="form-control" rows="3"  ></textarea>
             <br>
-            Důležité: <input type="checkbox" name="important" value="true"> 
+            </div>
+            <div class="form-group">
+                <label for="imp">Důležité:</label>
+                <input class="form-control" id="imp" type="checkbox" name="important" value="true"> 
+            </div>
+            <hr>
             <div align="right">
                 <button type="reset" class="btn btn-danger">Smazat</button>
             <button type="submit" class="btn btn-primary">Uložit</button>
@@ -62,7 +98,8 @@ $conn->close();
      
      </div>
      </p>
-    </div>
+        
+
     <div class="row col-sm-3 col-xs-0"></div>
 
 </html>
